36071b5f1d03dce676bd228f7a08a56e2c136e84
Dara McHugh <dara.mchugh@gmail.com>
2018-09-29 23:12:50 +0100

    Find node B with greatest connecting edge weight to node A

--- graphs/README.md
+++ graphs/README.md
@@ -2,12 +2,19 @@
 Some work exploring graph calculations and search in Prolog.
 
 ## Bidirectional
-A graph supporting bidirectional lookup
+A graph supporting bidirectional lookup, using a recursive search with backtrace.
+```prolog
     ?- path_available(a, b, _).
     true.
     ?- path_available(a, d, []).
     true.
     ?- path_available(d, a, []).
     true.
+```
     
 ## Weighted
+A weighted graph with function to extract the connected node with greatest edge weight.
+```prolog
+    ?- heaviestConnectingNode(b, X).
+    X = d.
+```
--- graphs/weighted.pl
+++ graphs/weighted.pl
@@ -9,6 +9,21 @@ connected(X, Y) :- link(Y, X, _).
 connected(X, Y, W) :- link(X, Y, W).
 connected(X, Y, W) :- link(Y, X, W).
 
+reverse([],[]).
+reverse([H|T], R) :-
+    reverse(T, RTemp), append(RTemp, [H], R).
+
 % Find all the connecting nodes and their weights
 linkedNodes(X, L) :-
-    findall([Y, W], connected(Y, X, W), L).
+    findall(W-Y, connected(Y, X, W), L).
+
+% Sort the connecting nodes by weight
+weightedNodes(X, SP) :-
+    linkedNodes(X, L), keysort(L, SPAsc), reverse(SPAsc, SP).
+
+transpose(X-Y, Y-X).
+splitKey(X-_, X).
+
+heaviestConnectingNode(X, Y) :-
+    weightedNodes(X, SP), SP = [HP|_],
+    transpose(HP, H), splitKey(H, Y).
