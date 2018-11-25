e4cc7dd9cd227091938e1cadefa2c05aafb69248
Dara McHugh <dara.mchugh@gmail.com>
2018-11-25 22:39:54 +0000

    Wrote some methods for inventory, recursive location, opening and closing doors, turning on and off flashlights

--- adventure/nani/nani.pl
+++ adventure/nani/nani.pl
@@ -1,6 +1,7 @@
 :- dynamic here/1.
 :- dynamic have/1.
 :- dynamic location/2.
+:- dynamic off/1.
 
 here(kitchen).
 
@@ -16,14 +17,40 @@ door(hall, 'dining room').
 door(kitchen, cellar).
 door('dining room', kitchen).
 
+door(office, hall, open).
+door(kitchen, office, closed).
+door(hall, 'dining room', closed).
+door(kitchen, cellar, closed).
+door('dining room', kitchen, closed).
+
+open(door(RoomA, RoomB)) :-
+    retract(door(RoomA, RoomB, closed)),
+    asserta(door(RoomA, RoomB, open)),
+    write('Door opened').
+
+closed(door(RoomA, RoomB)) :-
+    retract(door(RoomA, RoomB, open)),
+    asserta(door(RoomA, RoomB, closed)),
+    write('Door closed').
+
 location(desk, office).
 location(apple, kitchen).
 location(flashlight, desk).
+location(envelope, desk).
+location(stamp, envelope).
+location(key, envelope).
 location('washing machine', cellar).
 location(nani, 'washing machine').
 location(broccoli, kitchen).
 location(crackers, kitchen).
 location(computer, office).
+off(flashlight).
+
+is_contained_in(T1, T2) :-
+    location(T1, T2).
+
+is_contained_in(T1, T2) :-
+    location(T1, X), is_contained_in(X, T2).
 
 edible(apple).
 edible(crackers).
@@ -76,7 +103,17 @@ goto(Place) :-
 
 can_go(Place) :-
     here(X),
-    connected(X, Place).
+    connected(X, Place),
+    door(X, Place, open).
+
+can_go(Place) :-
+    here(X),
+    connected(X, Place),
+    door(X, Place, closed),
+    write("The door between the "), write(X),
+    write(" and the "), write(Place),
+    write(" is closed!"),
+    fail.
 
 can_go(Place) :-
     here(X),
@@ -94,7 +131,7 @@ take(Thing) :-
 
 can_take(Thing) :-
     here(Place),
-    location(Thing, Place).
+    is_contained_in(Thing, Place).
 
 can_take(Thing) :-
     write('There is no'), write(Thing),
@@ -105,3 +142,38 @@ take_object(Thing) :-
     retract(location(Thing, _)),
     asserta(have(Thing)),
     write(Thing), write(' taken'), nl.
+
+put(Thing) :-
+    retract(have(Thing)),
+    here(Place),
+    asserta(location(Thing, Place)).
+
+inventory :-
+    write('You have:'), nl,
+    have(Thing), write(Thing), nl, fail.
+
+turn_on(Thing) :-
+    off(Thing),
+    retract(off(Thing)),
+    asserta(on(Thing)),
+    write(Thing), write(' on!').
+
+turn_on(Thing) :-
+    on(Thing),
+    write(Thing), write(' already on!').
+
+turn_on(_) :-
+    write("You can't turn that on!"), fail.
+
+turn_off(Thing) :-
+    on(Thing),
+    retract(on(Thing)),
+    asserta(off(Thing)),
+    write(Thing), write(' off!').
+
+turn_off(Thing) :-
+    off(Thing),
+    write(Thing), write(' already off!').
+
+turn_off(_) :-
+    write("You can't turn that off!").
