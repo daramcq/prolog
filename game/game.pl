a5b27a6ef676bbc855bd09292f70f62df57fc1ec
Dara McHugh <1451455+daramcq@users.noreply.github.com>
2020-05-06 22:29:25 +0100

    Fix loop to run with non-global state, add some actions using State

--- game/game.pl
+++ game/game.pl
@@ -1,5 +1,3 @@
-
-
 state(State):-
     State = [player-[location-x, inventory-[torch, snorkel]],
              rooms-[room-[description-[this, is, a, place], items-[trowel]]]].
@@ -7,21 +5,47 @@ state(State):-
 player_info(State, PlayerInfo) :-
     car(State, _-PlayerInfo).
 
-inventory(State, Inventory) :-
+
+write_list([]).
+write_list([LastElement]) :-
+    write(LastElement),
+    write('.').
+
+write_list([H|T]) :-
+    write(H),
+    write(', '),
+    write_list(T).
+
+get_inventory(State, Inventory) :-
     player_info(State, PlayerInfo),
     cdr(PlayerInfo, InfoTail),
     car(InfoTail, _-Inventory).
 
-get_inventory(Inventory) :-
-    state(S),
-    inventory(S, Inventory).
+inventory(State) :-
+    get_inventory(State, Inventory),
+    write_inventory(Inventory).
 
-loop :-
+write_inventory([]):-
+    write('You have nothing!').
+
+write_inventory(Inventory):-
+    write('Your inventory is: '),
+    write_list(Inventory).
+
+get_location(State, Location) :-
+    player_info(State, PlayerInfo),
+    car(PlayerInfo, _-Location).
+
+play :-
+    state(StartingState),
+    loop(StartingState).
+
+loop(State) :-
     repeat,
     prompt_input(InputList),
-    parse_command(InputList, Action, Object),
-    call(Action, Object).
-    !.
+    Command =.. InputList,
+    call(Command, State),
+    loop(State).
 
 car(List, Head) :-
     List = [Head|_].
@@ -29,18 +53,29 @@ car(List, Head) :-
 cdr(List, Tail) :-
     List = [_|Tail].
 
-parse_list(List, Action, Object) :-
-    List = [Action|Tail],
-    car(Tail, Object).
+parse_command(List, Action, Object) :-
+    car(List, Action),
+    cdr(List, Object).
 
-parse_command(X, Action, Object) :-
-    parse_list(X, Action, Object).
-
-move(X) :-
+move(X, State) :-
+    get_location(State, CurrentLocation),
+    write('You are in, '),
+    write(CurrentLocation),
+    write('.'),
+    nl,
     write('You are moving '),
     write(X).
 
+look(State) :-
+    get_location(State, CurrentLocation),
+    write('You are in '),
+    write(CurrentLocation),
+    write('.'),
+    nl,
+    write('You are looking').
+
 prompt_input(UserInputAtoms) :-
+    nl,
     ansi_format([bold, fg(white)], '[Game] >> ', []),
     read_line_to_string(user_input, UserInputString),
     string_lower(UserInputString, UserInputStringLower),
